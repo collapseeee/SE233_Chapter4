720e65f531fc1d98348fe718badf0b2a363e655b
COLLAPse <4174tt@gmail.com>
2025-09-03 13:38:48 +0700

    Case Study is done.

---
+++ src/main/java/se233/chapter4/controller/DrawingLoop.java
@@ -0,0 +1,47 @@
+package se233.chapter4.controller;
+
+import se233.chapter4.model.GameCharacter;
+import se233.chapter4.view.GameStage;
+
+public class DrawingLoop implements Runnable {
+    private GameStage gameStage;
+    private int frameRate;
+    private float interval;
+    private boolean running;
+    public DrawingLoop(GameStage gameStage) {
+        this.gameStage = gameStage;
+        frameRate = 60;
+        interval = 1000.0f / frameRate; // 1000 ms = 1 sec
+        running = true;
+    }
+    private void checkDrawCollision(GameCharacter gameCharacter) {
+        gameCharacter.checkReachGameWall();
+        gameCharacter.checkReachHighest();
+        gameCharacter.checkReachFloor();
+    }
+    private void paint(GameCharacter gameCharacter) {
+        gameCharacter.repaint();
+    }
+    @Override
+    public void run() {
+        while (running) {
+            float time = System.currentTimeMillis();
+            checkDrawCollision(gameStage.getGameCharacter());
+            paint(gameStage.getGameCharacter());
+            time = System.currentTimeMillis() - time;
+            if (time < interval) {
+                try {
+                    Thread.sleep((long) (interval - time));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    Thread.sleep((long) (interval - (interval % time)));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
